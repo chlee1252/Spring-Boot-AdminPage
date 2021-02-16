1ca0dfa529d89db57ddf727541791b88b9a27203
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 18:27:26 +0900

    feat: add item api - post

---
+++ src/main/java/com/example/admin/controller/api/ItemApiController.java
@@ -0,0 +1,44 @@
+package com.example.admin.controller.api;
+
+import com.example.admin.ifs.CrudInterface;
+import com.example.admin.model.network.Header;
+import com.example.admin.model.network.request.ItemApiRequest;
+import com.example.admin.model.network.response.ItemApiResponse;
+import com.example.admin.service.ItemApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
+    @Override
+    @PostMapping("")
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+        log.info("{}", request);
+        return itemApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("")
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+}
